66287148c8025771102aa5f1b84683a7dfc2c87f
Shivani Roy <shivani@Shivanis-MacBook-Air-2.local>
2023-12-13 14:43:28 -0500

    git hub pages

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/contact" element={<Contact/>}/>
-          <Route path="/home" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
         </Routes>
       
     </div>
